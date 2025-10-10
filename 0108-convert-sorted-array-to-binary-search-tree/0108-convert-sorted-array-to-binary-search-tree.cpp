bdd1fe4eedb93405925bdc4d3948042dde545db4
Shubhamsingh520 <shubhamsinghrajput419@gmail.com>
2025-10-10 15:56:56 +0530

    Time: 1 ms (76.17%), Space: 23.1 MB (22.35%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+    TreeNode* helper(vector<int>& nums, int st, int end) {
+        if(st > end) {
+            return nullptr;
+        }
+        int mid = st + (end - st) / 2;
+        TreeNode* curr = new TreeNode(nums[mid]); 
+        curr->left =  helper(nums, st, mid-1); 
+        curr->right =  helper(nums, mid+1, end);
+        return curr;
+        
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root == nullptr) {
+            return {};
+        }
+        vector<int> result;
+        
+        result.push_back(root->val); 
+
+        vector<int> left = preorderTraversal(root->left);
+        result.insert(result.end(), left.begin(), left.end());
+
+        vector<int> right = preorderTraversal(root->right);
+        result.insert(result.end(), right.begin(), right.end());
+        return result;
+
+        
+    }
+};
