085d3f6c697fa0d4055d2e6c44be2e02705dabe6
Shubhamsingh520 <shubhamsinghrajput419@gmail.com>
2025-10-13 23:59:44 +0530

    Time: 7 ms (7.85%), Space: 13.6 MB (91.49%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -5,7 +5,9 @@ public:
         int n = nums.size();
         for(int i=0;i<n;i++){
             pq.push({nums[i],i});
-            if(pq.size() > k) pq.pop();
+            if(pq.size() > k) {
+                pq.pop();
+            }
         }
         vector<int>ans;
         while(!pq.empty()){
