24216207bc2ad9946f82564bd7886f55e1a19939
Shubhamsingh520 <shubhamsinghrajput419@gmail.com>
2025-11-28 21:49:29 +0530

    Time: 7 ms (66.88%), Space: 30.8 MB (76.88%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -5,25 +5,16 @@ public:
         unordered_set<int> s;
         int n = digits.size();
         for(int i=0; i<n; i++) {
-            int a = digits[i];
-            if(a == 0) {
-                continue;
-            }
             for(int j=0; j<n; j++) {
-                if(j == i) {
-                    continue;
-                }
-                int b = digits[j];
                 for(int k=0; k<n; k++) {
-                    if(k == i || k == j) {
-                        continue;
+                    if(i != j && j != k && i != k) {
+                        if(digits[i] != 0) {
+                            if(digits[k] % 2 == 0) {
+                                int number = digits[i] * 100 + digits[j] * 10 + digits[k];
+                                s.insert(number);
+                            }
+                        }
                     }
-                    int c = digits[k];
-                    if(c % 2 != 0) {
-                        continue;
-                    }
-                    int number = a*100 + b*10 + c;
-                    s.insert(number);
                 }
             }
         }
