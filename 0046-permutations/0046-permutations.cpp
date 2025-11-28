626e7a59397d4d53e354a6e770bb9ce0f8158966
Shubhamsingh520 <shubhamsinghrajput419@gmail.com>
2025-11-28 20:47:38 +0530

    Time: 0 ms (100%), Space: 10.5 MB (87.54%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -7,14 +7,14 @@ public:
 
     }
     void backtrack(vector<int>& nums, int start, vector<vector<int>>& result) {
-        if (start == nums.size()) {
+        if(start == nums.size()) {
             result.push_back(nums);
             return;
         }
-        for(int i = start; i<nums.size(); i++) {
+        for(int i=start; i<nums.size(); i++) {
             swap(nums[start], nums[i]);
             backtrack(nums, start+1, result);
-            swap(nums[start], nums[i]); 
+            swap(nums[start], nums[i]);
         }
     }
 };
