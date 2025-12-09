0a340e57621991db6c9da0ce0d3323f9dc9f4069
Shubhamsingh520 <shubhamsinghrajput419@gmail.com>
2025-12-09 21:24:36 +0530

    Time: 132 ms (50%), Space: 17.4 MB (58.45%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,24 +3,29 @@ public:
     bool canPartition(vector<int>& nums) {
         int total = 0;
         for (int x : nums) total += x;
+
         if (total % 2 != 0) return false;
 
         int target = total / 2;
-        int n = nums.size();
 
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-        return solve(0, target, nums, dp);
+        vector<vector<bool>> dp(nums.size() + 1, vector<bool>(target + 1, false));
+
+        return solve(nums, target, dp);
     }
 
-    bool solve(int i, int sum, vector<int>& nums, vector<vector<int>>& dp) {
-        if (sum == 0) return true;
-        if (i >= nums.size() || sum < 0) return false;
+    bool solve(vector<int>& nums, int sum, vector<vector<bool>>& dp) {
+        int n = nums.size();
+        for (int i = 0; i <= n; i++) dp[i][0] = true;
+        for (int i = 1; i <= n; i++) {
+            int val = nums[i - 1];
+            for (int j = 1; j <= sum; j++) {
+                bool npick = dp[i - 1][j];
+                bool pick = false;
+                if (val <= j) pick = dp[i - 1][j - val];
+                dp[i][j] = pick || npick;
+            }
+        }
 
-        if (dp[i][sum] != -1) return dp[i][sum];
-
-        bool pick = solve(i + 1, sum - nums[i], nums, dp);
-        bool npick = solve(i + 1, sum, nums, dp);
-
-        return dp[i][sum] = (pick || npick);
+        return dp[n][sum];
     }
 };
