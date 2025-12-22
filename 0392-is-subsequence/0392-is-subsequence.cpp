2a9cf3ec2aeedee9db2a8ad2303f35505b45d997
Shubhamsingh520 <shubhamsinghrajput419@gmail.com>
2025-12-22 14:33:36 +0530

    Time: 124 ms (2.47%), Space: 290.7 MB (5.33%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
     bool helper(string s, string t, int n, int m) {
         if(n == 0) {
             return true;
@@ -7,13 +8,17 @@ public:
         if(m == 0) {
             return false;
         }
+        if(dp[n][m] != -1) {
+            return dp[n][m];
+        }
         if(s[n-1] == t[m-1]) {
-            return helper(s, t, n-1, m-1);
+            return dp[n][m] = helper(s, t, n-1, m-1);
         } else {
-            return helper(s, t, n, m-1);
+            return dp[n][m] = helper(s, t, n, m-1);
         }
     }
     bool isSubsequence(string s, string t) {
+        dp.assign(s.size() + 1, vector<int>(t.size() + 1, -1));
         return helper(s, t, s.size(), t.size());
         
     }
