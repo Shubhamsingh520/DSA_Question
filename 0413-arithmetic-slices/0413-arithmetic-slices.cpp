5e273df818d674f043996d734e07538da8d184b0
Shubhamsingh520 <shubhamsinghrajput419@gmail.com>
2025-12-26 17:33:49 +0530

    Time: 0 ms (100%), Space: 11 MB (27.94%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     int solve(int i, vector<int>& nums, vector<int>& dp) {
          
-        if(i < 2) return 0;
+        if(i < 2) return 0; 
         
-        if(dp[i] != -1) return dp[i];
+        if(dp[i] != -1) return dp[i]; 
         
          
-        if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
-            dp[i] = 1 + solve(i-1, nums, dp);  
-        } else {
-            dp[i] = 0;  
-            solve(i-1, nums, dp);  
-        }
+        if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) { 
+            dp[i] = 1 + solve(i-1, nums, dp);   
+        } else { 
+            dp[i] = 0;   
+            solve(i-1, nums, dp);   
+        } 
         
-        return dp[i];
-    }
+        return dp[i]; 
+    } 
 
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        int total = 0;
+    int numberOfArithmeticSlices(vector<int>& nums) { 
+        int n = nums.size(); 
+        vector<int> dp(n, -1); 
+        int total = 0; 
         
         
-        for(int i = 2; i < n; i++) {
-            total += solve(i, nums, dp);
-        }
-        return total;
-    }
-};
+        for(int i = 2; i < n; i++) { 
+            total += solve(i, nums, dp); 
+        } 
+        return total; 
+    } 
+}; 
