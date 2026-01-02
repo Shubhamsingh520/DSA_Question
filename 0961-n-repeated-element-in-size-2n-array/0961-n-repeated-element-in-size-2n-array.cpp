a19bf4d9a2c331a28262de5df1d3bdf1d517a515
Shubhamsingh520 <shubhamsinghrajput419@gmail.com>
2026-01-02 14:08:33 +0530

    Time: 15 ms (32.66%), Space: 34.2 MB (16.71%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for(int i=0; i<nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        for(auto &p : m) {
+            if(p.second > 1) {
+                return p.first;
+            }
+        }
+        return -1; 
+         
+    }
+};
