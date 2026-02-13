c4be869f4f982341a2a9b7da7435cb2085e4c65a
Shubhamsingh520 <shubhamsinghrajput419@gmail.com>
2026-02-13 17:30:14 +0530

    Time: 6 ms (94.3%), Space: 34.8 MB (64.25%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,31 +3,37 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<int>> level(n, vector<int> (m, 1e6)); 
-        queue<pair<int, int>> q;
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(mat[i][j] == 0) { 
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+        queue<pair<int,int>> q;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(mat[i][j] == 0) {
+                    dist[i][j] = 0;
                     q.push({i, j});
-                    level[i][j] = 0;
                 }
             }
-        } 
+        }
+
         int dr[4] = {-1, 1, 0, 0};
         int dc[4] = {0, 0, -1, 1};
         while(!q.empty()) {
             auto [r, c] = q.front();
             q.pop();
-            for(int k=0; k<4; k++) {
+
+            for(int k = 0; k < 4; k++) {
                 int nr = r + dr[k];
                 int nc = c + dc[k];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && level[nr][nc] > level[r][c] + 1) {
-                    level[nr][nc] = level[r][c] + 1;
-                    q.push({nr, nc});
+
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m) {
+                    if(dist[nr][nc] > dist[r][c] + 1) {
+                        dist[nr][nc] = dist[r][c] + 1;
+                        q.push({nr, nc});
+                    }
                 }
             }
         }
-        return level;
+
+        return dist;
     }
 };
